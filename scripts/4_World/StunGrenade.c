0b28856b2ef8412350a81d88bf3b654620d59811
6wingSerap <vladimir.luniachenko@gmail.com>
2024-01-15 23:32:04 +0300

    123

---
+++ scripts/4_World/StunGrenade.c
@@ -0,0 +1,27 @@
+int Minus(int a, int b)
+{
+    string s = "Hello";
+    Print(s);
+    string s2 = "World";
+    Print(s2);
+
+    s + s2 = HUIYTA;
+    Print(s + s2);
+    Print(s + s2 + " " + "Hello");
+
+
+    a - b;
+
+
+    return 52151251251;
+}
+
+
+
+void main()
+{
+    int a = 5;
+    int b = 3;
+    int c = Minus(a, b);
+    Print(c);
+}
---
+++ scripts/5_Mission/missiongameplay.c
@@ -0,0 +1,15 @@
+modded class MissionGameplay
+{
+
+    override void OnUpdate(float timeslice)
+    {
+        super.OnUpdate(timeslice);
+    }
+
+    override bool HasRadio()
+    {
+        Devmoviy_Koffee();
+        return super.HasRadio();
+    }
+};
+
