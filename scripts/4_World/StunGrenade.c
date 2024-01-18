6ab1c77144b0a7338e72b9d52133dc7daec4b3cc
6wingSerap <vladimir.luniachenko@gmail.com>
2024-01-18 20:52:08 +0300

    123

--- scripts/4_World/StunGrenade.c
+++ scripts/4_World/StunGrenade.c
@@ -1,16 +1,22 @@
-void Prefix(string odin, string dva =="[c]",srting tri == "[s]")
+void Prefix(string odin, string dva == "[c]", srting tri == "[s]")
 {
-    if(IsServer == true)
-    Print(tri+odin)
-    else(IsServver ==true)
-    Print(dva+odin);
+    
+    if (IsServer == true)
+    {
+        Print(tri + odin);
+    }
+    else
+    {
+        Print(dva + odin);
+    }
 }
 
 void main()
 {
     Prefix("MyGameStart");
+    Prefix("MyGameStart", "[HUI]", "[PISOS]");
 
-   
+
 }
 
 
