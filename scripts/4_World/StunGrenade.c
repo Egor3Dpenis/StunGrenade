9fcdd8b81cb55886c81d6af0782ba2b479ea76d2
Egor3Dpenis <pepasvinka20@gmail.com>
2024-01-18 19:48:41 +0200

    penis

--- scripts/4_World/StunGrenade.c
+++ scripts/4_World/StunGrenade.c
@@ -1,35 +1,16 @@
-int Minus(int a, int b)
+void Prefix(string odin, string dva =="[c]",srting tri == "[s]")
 {
-    int result = a - b;
-    return result;
+    if(IsServer == true)
+    Print(tri+odin)
+    else(IsServver ==true)
+    Print(dva+odin);
 }
-
-int stepen (int charlie)
-{
-    int d = charlie^charlie;
-    return d;
-}
-
-string skobki(string alfa)
-{
-    string beta = "[";
-    string gamma = "]";
-    string Epsilon = beta+alfa+gamma;
-    return Epsilon;
-}
-
 
 void main()
 {
-    int a = 5;
-    int b = 3;
-    int c = Minus(a, b);
-    Print(c);
-    int alfa = 7;
-    int gamma = stepen(alfa); 
-    string alfa = "Penis";
-    string andrej = skobki(alfa);
+    Prefix("MyGameStart");
 
+   
 }
 
 
